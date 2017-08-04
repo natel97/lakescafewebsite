3766a212479ee0fa89c555b1b4f893a4818587ce
Nathanial Lubitz <nate.lubitz@gmail.com>
2017-08-03 22:50:46 -0500

    Fix missing json

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root 'sections#index'
   
   resources :sections
+  resources :items, :defaults => { :format => :json}
+  resources :headings, :defaults => {:format => :json}
+  resources :sub_items, :defaults => {:format => :json}
   get 'onepage', to: 'headings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
