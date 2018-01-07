8b2f01bdabe95ee727cea9ee9665a7c1ad7d936d
Nathanial Lubitz <nate.lubitz@gmail.com>
2018-01-07 15:43:30 -0600

    Add image to carousel

--- app/admin/carousel_image.rb
+++ app/admin/carousel_image.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register CarouselImage do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
- permit_params :image_url, :description, :visible
+ permit_params :image_url, :description, :visible, :image
 #
 # or
 #
@@ -11,5 +11,26 @@ ActiveAdmin.register CarouselImage do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+
+
+form do |f|
+  f.inputs "Section Details" do
+    f.input :image_url
+    f.input :description
+    f.input :visible
+    f.input :image, :required => true, :as => :file
+  end
+  f.actions
+ end
+
+show do |ad|
+  attributes_table do
+    row :description
+    row :visible
+    row :image do
+      image_tag(ad.image.url(:original), :style => "width: 640px; height: 480px;")
+    end
+  end
+ end
 
 end
