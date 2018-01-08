a58d03d8da8fcb4212b30c886f8f4df221d1f093
Nathanial Lubitz <nate.lubitz@gmail.com>
2018-01-08 15:17:17 -0600

    bugfix

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,9 +3,9 @@ class Section < ApplicationRecord
   has_many :items
   do_not_validate_attachment_file_type :image
   has_attached_file :image,
-    :storage => :google_drive,
-    :google_drive_credentials => {"#{Rails.root}/config/google_drive.yml"},
-    :google_drive_options => {
-    :public_folder_id => "1rwz3Znw3nWR2CFNP9Oli0d0gJ8i58_8_"
-  }
+                    storage: :google_drive,
+                    google_drive_credentials: "#{Rails.root}/config/google_drive.yml",
+                    google_drive_options: {
+                      public_folder_id: '1rwz3Znw3nWR2CFNP9Oli0d0gJ8i58_8_'
+                    }
 end
